1f58d614aea5e7f102eeb0cd05b5b2e89a863aec
Thierry Sansaricq <thierry@Thierrys-MBP.lan>
2015-07-21 10:33:43 -0400

    Initial Commit

---
+++ SimpleAVEngine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleAVEngine
+//
+//  Created by Thierry Sansaricq on 7/21/15.
+//  Copyright (c) 2015 Thierry Sansaricq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleAVEngine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleAVEngine
+//
+//  Created by Thierry Sansaricq on 7/21/15.
+//  Copyright (c) 2015 Thierry Sansaricq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
